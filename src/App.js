1816166c35659a2a475cc3bb89a508c4ffc72961
Your Name <you@example.com>
2024-06-24 10:21:30 +0100

    friday hw solution

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Characters from "./components/Characters";
 import CharactersControls from "./components/CharactersControls";
 
-class App extends Component {
-  state = {};
+const App = () => {
+  const [simpsons, setSimpsons] = useState();
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState();
 
-  componentDidMount() {
-    this.getSimpsons();
-  }
-
-  getSimpsons = async () => {
+  const getSimpsons = async () => {
     try {
       const { data } = await axios.get(
         `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
@@ -20,66 +18,68 @@ class App extends Component {
       data.forEach((element, index) => {
         element.id = index + 1;
       });
-      this.setState({ simpsons: data });
+      setSimpsons(data);
     } catch (e) {
-      this.setState({ error: "Api Down" });
+      setError(e);
     }
   };
 
-  onLikeToggle = (id) => {
-    const simpsons = [...this.state.simpsons];
-    const indexOf = simpsons.findIndex((item) => {
+  useEffect(() => {
+    getSimpsons();
+  }, []);
+
+  const onLikeToggle = (id) => {
+    const _simpsons = [...simpsons];
+    const indexOf = _simpsons.findIndex((item) => {
       return item.id === id;
     });
     if (indexOf === -1) {
       console.log("This should almost never occur");
       return;
     }
-    simpsons[indexOf].liked = !simpsons[indexOf].liked;
+    _simpsons[indexOf].liked = !_simpsons[indexOf].liked;
 
-    this.setState({ simpsons });
+    setSimpsons(_simpsons);
   };
 
-  onDelete = (id) => {
-    const simpsons = [...this.state.simpsons];
-    const indexOf = simpsons.findIndex((item) => {
+  const onDelete = (id) => {
+    const _simpsons = [...simpsons];
+    const indexOf = _simpsons.findIndex((item) => {
       return item.id === id;
     });
     if (indexOf === -1) {
       console.log("This should almost never occur");
       return;
     }
-    simpsons.splice(indexOf, 1);
-    this.setState({ simpsons });
+    _simpsons.splice(indexOf, 1);
+    setSimpsons(_simpsons);
   };
 
-  getTotalLiked = () => {
-    const liked = this.state.simpsons.filter((item) => {
+  const getTotalLiked = () => {
+    const liked = simpsons.filter((item) => {
       return item.liked;
     });
 
     return <h1>Total liked: {liked.length}</h1>;
   };
 
-  onFormEvent = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value,
-    });
+  const onFormEvent = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  getSimpsonsToDisplay = () => {
+  const getSimpsonsToDisplay = () => {
     //filter based on search
-    let filtered = [...this.state.simpsons];
-    if (this.state.text) {
+    let filtered = [...simpsons];
+    if (formData.text) {
       filtered = filtered.filter((item) => {
         return item.character
           .toLowerCase()
-          .includes(this.state.text.toLowerCase());
+          .includes(formData.text.toLowerCase());
       });
     }
 
     //if no resuls, return all results
-    filtered = filtered.length ? filtered : this.state.simpsons;
+    filtered = filtered.length ? filtered : simpsons;
 
     //sort the data
     filtered.sort((a, b) => {
@@ -92,37 +92,156 @@ class App extends Component {
       return 0;
     });
 
-    if (this.state.select === "DESC") {
+    if (formData.select === "DESC") {
       filtered.reverse();
     }
 
     return filtered; //either everything or if search term then filtered
   };
 
-  render() {
-    console.log(this.state);
-    const { simpsons, error } = this.state;
-
-    if (error) {
-      return <h1>API down, try later!</h1>;
-    }
-
-    if (!simpsons) {
-      return <p>Loading...</p>;
-    }
-
-    return (
-      <>
-        <CharactersControls callback={this.onFormEvent} />
-        {this.getTotalLiked()}
-        <Characters
-          characters={this.getSimpsonsToDisplay()}
-          onLikeToggle={this.onLikeToggle}
-          onDelete={this.onDelete}
-        />
-      </>
-    );
+  if (error) {
+    return <h1>API down, try later!</h1>;
   }
-}
+
+  if (!simpsons) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <CharactersControls callback={onFormEvent} />
+      {getTotalLiked()}
+      <Characters
+        characters={getSimpsonsToDisplay()}
+        onLikeToggle={onLikeToggle}
+        onDelete={onDelete}
+      />
+    </>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+
+//   state = {};
+
+//   componentDidMount() {
+//     this.getSimpsons();
+//   }
+
+//   getSimpsons = async () => {
+//     try {
+//       const { data } = await axios.get(
+//         `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
+//       );
+//       //fixes api data
+//       data.forEach((element, index) => {
+//         element.id = index + 1;
+//       });
+//       this.setState({ simpsons: data });
+//     } catch (e) {
+//       this.setState({ error: "Api Down" });
+//     }
+//   };
+
+//   onLikeToggle = (id) => {
+//     const simpsons = [...this.state.simpsons];
+//     const indexOf = simpsons.findIndex((item) => {
+//       return item.id === id;
+//     });
+//     if (indexOf === -1) {
+//       console.log("This should almost never occur");
+//       return;
+//     }
+//     simpsons[indexOf].liked = !simpsons[indexOf].liked;
+
+//     this.setState({ simpsons });
+//   };
+
+//   onDelete = (id) => {
+//     const simpsons = [...this.state.simpsons];
+//     const indexOf = simpsons.findIndex((item) => {
+//       return item.id === id;
+//     });
+//     if (indexOf === -1) {
+//       console.log("This should almost never occur");
+//       return;
+//     }
+//     simpsons.splice(indexOf, 1);
+//     this.setState({ simpsons });
+//   };
+
+//   getTotalLiked = () => {
+//     const liked = this.state.simpsons.filter((item) => {
+//       return item.liked;
+//     });
+
+//     return <h1>Total liked: {liked.length}</h1>;
+//   };
+
+//   onFormEvent = (e) => {
+//     this.setState({
+//       [e.target.id]: e.target.value,
+//     });
+//   };
+
+//   getSimpsonsToDisplay = () => {
+//     //filter based on search
+//     let filtered = [...this.state.simpsons];
+//     if (this.state.text) {
+//       filtered = filtered.filter((item) => {
+//         return item.character
+//           .toLowerCase()
+//           .includes(this.state.text.toLowerCase());
+//       });
+//     }
+
+//     //if no resuls, return all results
+//     filtered = filtered.length ? filtered : this.state.simpsons;
+
+//     //sort the data
+//     filtered.sort((a, b) => {
+//       if (a.character > b.character) {
+//         return 1;
+//       }
+//       if (a.character < b.character) {
+//         return -1;
+//       }
+//       return 0;
+//     });
+
+//     if (this.state.select === "DESC") {
+//       filtered.reverse();
+//     }
+
+//     return filtered; //either everything or if search term then filtered
+//   };
+
+//   render() {
+//     console.log(this.state);
+//     const { simpsons, error } = this.state;
+
+//     if (error) {
+//       return <h1>API down, try later!</h1>;
+//     }
+
+//     if (!simpsons) {
+//       return <p>Loading...</p>;
+//     }
+
+//     return (
+//       <>
+//         <CharactersControls callback={this.onFormEvent} />
+//         {this.getTotalLiked()}
+//         <Characters
+//           characters={this.getSimpsonsToDisplay()}
+//           onLikeToggle={this.onLikeToggle}
+//           onDelete={this.onDelete}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
